d7fa0db2964158f40aa246f0a5cd8b1a9ad08749
Farez Prieto <kyo20052@gmail.com>
2021-08-25 21:49:25 -0500

    Ajuste tamaño logo login

--- application/views/login/home.php
+++ application/views/login/home.php
@@ -3,10 +3,10 @@
 <script id="metamorph-1-start" type="text/x-placeholder"></script><script id="metamorph-21-start" type="text/x-placeholder"></script>
   <div class="row">
     <div class="col-lg-12">
-        <div class="container-fluid text-center" style="margin-top:0%">
+        <div class="container-fluid text-center" style="margin-top:5%">
               <form class="form-signin" data-ember-action="2" id="formLogin" ng-submit="loginInApp()">
                 <!--<h2 class="form-signin-heading"><?php echo lang("tituloBienvenida") ?></h2>-->
-                <img src="<?php echo base_url()?>res/img/logo.png?<?php echo rand(0,1000)?>" width="auto"><br><br>
+                <img src="<?php echo base_url()?>res/img/logo.png?<?php echo rand(0,1000)?>" width="100%"><br><br>
                 <!-- <small class="text-muted"><?php echo lang("textoBienvenida") ?></small> -->
                 <div class="row-picture" style="margin:10% 0 0 0">
                   <img class="img img-circle" id="miniatura" ng-src="{{fotoLogin}}" alt="icon" width="35%">
